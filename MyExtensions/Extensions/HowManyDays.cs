721c74c22f51a8c839691771f3571fe78ba0e141
Cagatay ONDER <cagatay-onder@hotmail.com>
2021-06-02 22:09:33 +0300

    Final

---
+++ MyExtensions/Extensions/HowManyDays.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyExtensions.Extensions
+{
+    public static class HowManyDays
+    {
+        public static int CalculateDays(this DateTime bornDate)
+{
+            TimeSpan difference = DateTime.Now - bornDate;
+            return difference.Days;
+        }
+    }
+}
---
+++ MyExtensions/Program.cs
@@ -0,0 +1,15 @@
+﻿using MyExtensions.Extensions;
+using System;
+
+namespace MyExtensions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date = new DateTime(1994, 06, 20);
+            int days = date.CalculateDays();
+            Console.WriteLine( days +" gündür hayattasın :)");
+        }
+    }
+}
